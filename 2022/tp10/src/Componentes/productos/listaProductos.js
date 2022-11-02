b26492408568cf94aded41b3bf6fe69a6d92637a
GaloDoublier <81277730+GaloDoublier@users.noreply.github.com>
2022-11-02 09:03:26 -0300

    Terminado con mas productos

--- 2022/tp10/src/Componentes/productos/listaProductos.js
+++ 2022/tp10/src/Componentes/productos/listaProductos.js
@@ -5,7 +5,7 @@ let listaProductos = [
         descripcion:"Este baño es un baño excelente",
         precio:400,
         foto:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDV6veSl6n4uoboF8zophKwY0XXEVz8H5_Lw&usqp=CAU",
-        destacado:true
+        destacado:false
     },
     {
         nombre:"Baño de hielo",
@@ -19,14 +19,14 @@ let listaProductos = [
         descripcion:"Baño con mucho estilo para subir fotos a instagram",
         precio:600,
         foto:"https://pbs.twimg.com/media/E-zfnsEWUAMvIN_.jpg",
-        destacado:true
+        destacado:false
     },
     {
-        nombre:"Baño LECH",
-        descripcion:"lleno de ranas para limpiarse",
-        precio:1000000,
-        foto:"https://wiselwisel.com/wp-content/uploads/2022/09/tumblr_o918asBDxs1uluepno1_500.jpg",
-        destacado:true
+        nombre:"Baño doblado",
+        descripcion:"es un baño, pero doblado",
+        precio:600,
+        foto:"http://pm1.narvii.com/7450/cf56ce75becc8a46d867cfe7df087ab61282d833r1-739-415v2_uhq.jpg",
+        destacado:false
     },
     {
         nombre:"?",
@@ -34,6 +34,34 @@ let listaProductos = [
         precio:0,
         foto:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAlmUZ_swLd8TGctuM0qoYRIG-Iuvo5vCbFw&usqp=CAU",
         destacado:false
+    },
+    {
+        nombre:"Baño de FNAF",
+        descripcion:"Es una baño de un juego de miedo",
+        precio:760,
+        foto:"http://images3.memedroid.com/images/UPLOADED755/5ffda27b6c590.jpeg",
+        destacado:true
+    },
+    {
+        nombre:"Baño Hiroshima",
+        descripcion:"読んでいる同性愛者",
+        precio:3000,
+        foto:"https://www.laguiadelvaron.com/wp-content/uploads/2022/05/DuQfEkRX4AAJo86-525x700.jpg",
+        destacado:true
+    },
+    {
+        nombre:"Baño gamer",
+        descripcion:"No se por que hicieron este baño, pero me encanta",
+        precio:9999,
+        foto:"https://cl.buscafs.com/www.levelup.com/public/uploads/images/753970/753970.jpg",
+        destacado:true
+    },
+    {
+        nombre:"Baño pato donald",
+        descripcion:"tenes que ser valiente para defecar aca",
+        precio:200,
+        foto:"https://st3.idealista.com/news/archivos/styles/amp_1200x675_16_9/public/2018-10/el_pato_donald.jpg?VersionId=w9a.k7vWwIUzYxgTo9jcf7H0zd8API0f&itok=nm8kX6fN",
+        destacado:true
     },
 ]
 export default listaProductos;
